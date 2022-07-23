3d58d36c2d7339803b9acace1941c92ddef20e4c
gurumurthi97 <guru13041997@gmail.com>
2022-07-23 15:26:55 +0530

    I changed js code

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
+const displaymess=function (message){
+    document.querySelector('.message').textContent=message;
+}
+const number=function (num){
+   document.querySelector('.number').textContent;
+}
 let secretNumber=Math.floor(Math.random()*20)+1;
-document.querySelector('.number').textContent="?";
+number('?')
+
 let score=20;
 let highscore=0;
 document.querySelector('.check').addEventListener('click',function increment(){
    const guess=Number(document.querySelector('.guess').value);
-   
+  
 console.log(typeof guess,guess);
    if(!guess){
-      document.querySelector('.message').textContent="NO Value";
+      displaymess('No Number 😢')
+     
     }
   
   else if(guess === secretNumber){
-   document.querySelector('.number').textContent=secretNumber;
-      document.querySelector('.message').textContent='Congalutations';
+     number(secretNumber)
+      displaymess('Congagulations 👍');
       document.querySelector('body').style.backgroundColor="#60b347"
       document.querySelector('.number').style.width="30rem";
-      document.querySelector('.number').textContent=secretNumber;
       if(highscore<score){
          highscore=score;
          document.querySelector('.highscore').textContent=score;
       }
+    }else if(guess!=secretNumber){
+      score--;
+      document.querySelector('.score').textContent=score;
+      displaymess((guess>secretNumber)?'U are High':'u are low ');
     }
-    else if(guess>secretNumber){
-      if(guess>0){
-         score--;
-         document.querySelector('.score').textContent=score;
-         document.querySelector('.message').textContent='U are High';
-
-      }
-    }
-    else if(guess<secretNumber){
-      if(guess>0){
-         score--;
-         document.querySelector('.score').textContent=score;
-         document.querySelector('.message').textContent='u re loeeee';
-      }
-    }
+ 
 })
 
 
 document.querySelector('.again').addEventListener('click',function(){
    document.querySelector('body').style.backgroundColor="#333"
    document.querySelector('.number').style.width="15rem";
+   
    score=20;
    document.querySelector('.score').textContent=score;
    secretNumber=Math.floor(Math.random()*20)+1;
-   document.querySelector('.number').textContent="?";
+  
+   number('?')
    document.querySelector('.guess').value='';
+   displaymess('Start guessing...')
 })
